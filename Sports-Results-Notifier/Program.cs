0806a263685c6a8eaa666599575d32c347cbb075
Andrew Martinez <martinez.s.andrew@gmail.com>
2025-07-15 15:42:15 -0400

    Working on a console app that will scrape information from https://www.basketball-reference.com/boxscores/ and email a user with daily results.

---
+++ Sports-Results-Notifier/Program.cs
@@ -0,0 +1,23 @@
+﻿using HtmlAgilityPack;
+
+var url = "https://www.basketball-reference.com/boxscores/";
+HtmlWeb web = new HtmlWeb();
+HtmlDocument doc = web.Load(url);
+
+var tableNodeGameSummary = doc.DocumentNode.SelectSingleNode(
+    "//*[@id=\"content\"]/div/div/table[2]"
+);
+
+var tableNodeConfStandingsEast = doc.DocumentNode.SelectSingleNode(
+    "//*[@id=\"confs_standings_E\"]"
+);
+
+var tableNodeConfStandingsWest = doc.DocumentNode.SelectSingleNode(
+    "//*[@id=\"confs_standings_W\"]"
+);
+
+Console.WriteLine(tableNodeGameSummary.InnerText);
+Console.WriteLine(tableNodeConfStandingsEast.InnerText);
+Console.WriteLine(tableNodeConfStandingsWest.InnerText);
+
+Console.ReadKey();
